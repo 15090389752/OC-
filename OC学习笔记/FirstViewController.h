49536428019b2a1fc10afd61997a84abaf5f1d9c
人生正当年 <15090389752@163.com>
2019-12-13 10:53:31 +0800

    .

---
+++ OC学习笔记/FirstViewController.h
@@ -0,0 +1,17 @@
+//
+//  FirstViewController.h
+//  OC学习笔记
+//
+//  Created by huodada on 2019/12/12.
+//  Copyright © 2019 huodada. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface FirstViewController : UIViewController
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ OC学习笔记/UIDevice+TFDevice.h
@@ -0,0 +1,20 @@
+//
+//  UIDevice+TFDevice.h
+//  OC学习笔记
+//
+//  Created by huodada on 2019/12/12.
+//  Copyright © 2019 huodada. All rights reserved.
+//
+
+#import <AppKit/AppKit.h>
+
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface UIDevice (TFDevice)
+
+@end
+
+NS_ASSUME_NONNULL_END
